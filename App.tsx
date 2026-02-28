8ceec323c3f7f2e297c4437bb1bbf0caca09831f
sauravchauhan010 <49646008+sauravchauhan010@users.noreply.github.com>
2026-03-01 01:13:51 +0400

    Delete App.tsx

--- App.tsx
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { raynaProxy } from './_proxy';
-
-export default (req: VercelRequest, res: VercelResponse) =>
-  raynaProxy('/Tour/tourStaticDataById', req, res);
